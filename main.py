36c2e85cbf772b088135ce75a7c0419119f8ce52
Gustavo Matos <gustavofisica@gmail.com>
2022-02-27 20:29:14 -0300

    Changing font colors in console

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from cgitb import reset
 from wordle import Wordle
+from letter_state import LetterState
+from colorama import Fore
+from typing import List
 
 
 def main():
@@ -7,19 +9,46 @@ def main():
     wordle = Wordle("APPLE")
 
     while wordle.can_attempt:
-        x = input("Type your guess: ")
-        wordle.attempt(x)
+        x = input("\nType your guess: ")
         if len(x) != wordle.WORD_LENGTH:
-            print(f"Word must be {wordle.WORD_LENGTH} characters long!")
+            print(
+                Fore.RED + f"Word must be {wordle.WORD_LENGTH} characters long!" + Fore.RESET)
             continue
 
-        result = wordle.guess(x)
-        print(*result, sep="\n")
+        wordle.attempt(x)
+        display_results(wordle)
 
     if wordle.is_solved:
         print("You've solved the puzzle.")
     else:
         print("You failed to solve the puzzle!")
+
+
+def display_results(wordle: Wordle):
+    print("\nYour results so far...\n")
+    print(f"\nYou have {wordle.remaining_attempts} attempts remaining.\n")
+
+    for word in wordle.attempts:
+        result = wordle.guess(word)
+        colored_result_str = convert_result_to_color(result)
+        print(colored_result_str)
+
+    for _ in range(wordle.remaining_attempts):
+        print(" ".join(["_"] * wordle.WORD_LENGTH))
+
+
+def convert_result_to_color(result: List[LetterState]):
+    result_with_color = []
+    for letter in result:
+        if letter.is_in_position:
+            color = Fore.GREEN
+        elif letter.is_in_word:
+            color = Fore.YELLOW
+        else:
+            color = Fore.LIGHTRED_EX
+        colored_letter = color + letter.character + Fore.RESET
+        result_with_color.append(colored_letter)
+    return " ".join(result_with_color)
 
 
 if __name__ == "__main__":
--- wordle.py
+++ wordle.py
@@ -18,7 +18,7 @@ class Wordle:
     def guess(self, word: str):
         word = word.upper()
         result = []
-        
+
         for i in range(self.WORD_LENGTH):
             character = word[i]
             letter = LetterState(character)
